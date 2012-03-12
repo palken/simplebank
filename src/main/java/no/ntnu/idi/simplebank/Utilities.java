7c7a6355e10409adf0a3d3df786d982bb1270d45
Pål Thomassen <palt@stud.ntnu.no>
2012-03-12 13:29:04 +0100

    Fixes a nullpointer in getCurrentlyLoggedInUser

--- src/main/java/no/ntnu/idi/simplebank/Utilities.java
+++ src/main/java/no/ntnu/idi/simplebank/Utilities.java
@@ -6,22 +6,24 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 public class Utilities {
-	
+
 	public static String getCurrentlyLoggedInUser(HttpServletRequest request) {
 		Cookie[] cookies = request.getCookies();
-		for (Cookie cookie : cookies) {
-			if (cookie.getName().equals("logged_in_user")){
-				return cookie.getValue();
+		if (cookies != null) {
+			for (Cookie cookie : cookies) {
+				if (cookie.getName().equals("logged_in_user")) {
+					return cookie.getValue();
+				}
 			}
 		}
 		return null;
 	}
 
 	public static List<Account> getAccountsForUser(String currentlyLoggedInUser) {
-		
+
 		Database database = new Database();
 		return database.getAccountsForUsername(currentlyLoggedInUser);
-		
+
 	}
 
 }
